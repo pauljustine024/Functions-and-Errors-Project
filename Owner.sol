e6bb587913b6d714b3dc32fe776f74a0953944d0
pauljustine024 <128199663+pauljustine024@users.noreply.github.com>
2024-05-25 14:54:01 +0800

    Owner.sol

--- Owner.sol
+++ Owner.sol
@@ -1,21 +1,39 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
 contract Paul {
     uint256 public myNumber;
+    address public owner;
 
-    function setNumber(uint256 _num) external {
-        
-        if (_num == 0) {
-            revert("Number cannot be zero");
-        }
-        
+    event NumberSet(uint256 newNumber);
+    event NumberIncremented(uint256 incrementedBy, uint256 newNumber);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Caller is not the owner");
+        _;
+    }
+
+    modifier validNumber(uint256 _num) {
+        require(_num != 0, "Number cannot be zero");
+        require(_num <= 100, "Number is too large");
+        _;
+    }
+
+    function setNumber(uint256 _num) external onlyOwner validNumber(_num) {
         require(myNumber + _num > myNumber, "Overflow detected");
 
-        if (_num > 100) {
-            revert("Number is too large");
-        }
-
         myNumber = _num;
+        emit NumberSet(_num);
+    }
+
+    function incrementNumber(uint256 _num) external onlyOwner validNumber(_num) {
+        require(myNumber + _num > myNumber, "Overflow detected");
+
+        myNumber += _num;
+        emit NumberIncremented(_num, myNumber);
     }
 }
